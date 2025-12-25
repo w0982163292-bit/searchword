885bf458f0e5b9bc0da1808523ff45f954863923
hihi <236579512+w0982163292-bit@users.noreply.github.com>
2025-12-25 15:38:49 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,68 @@
+const wordData = [
+  { word: "笔电（笔记型电脑）", meaning: "laptop computer", example: "我想买一台新的笔电。(I want to buy a new laptop.)" },
+
+  { word: "产品", meaning: "product", example: "店员介绍了很多新产品。(The clerk introduced many new products.)" },
+
+  { word: "平板（平板电脑）", meaning: "tablet", example: "现在很多上班族都用平板工作。(Many office workers use tablets for work.)" },
+
+  { word: "差别", meaning: "difference", example: "笔电和平板有什么差别？(What’s the difference between a laptop and a tablet?)" },
+
+  { word: "说明", meaning: "to explain; explanation", example: "店员说明了笔电的功能。(The clerk explained the laptop’s features.)" },
+
+  { word: "功能", meaning: "function; feature", example: "这台笔电的功能很多。(This laptop has many functions.)" },
+
+  { word: "轻", meaning: "light (in weight)", example: "这台笔电很轻，带着很方便。(This laptop is light and easy to carry.)" },
+
+  { word: "出差", meaning: "business trip", example: "他常常出差，所以需要一台轻的笔电。(He often goes on business trips, so he needs a lightweight laptop.)" },
+
+  { word: "不如", meaning: "not as good as", example: "这台的速度不如那台快。(This one isn’t as fast as that one.)" },
+
+  { word: "大多数", meaning: "most; majority", example: "大多数客人都买这台。(Most customers buy this one.)" },
+
+  { word: "背包", meaning: "backpack", example: "笔电放在背包里很重。(Putting the laptop in the backpack feels heavy.)" },
+
+  { word: "不管", meaning: "no matter; regardless of", example: "不管价钱，我都想要好的品质。(No matter the price, I want good quality.)" },
+
+  { word: "跟", meaning: "to follow", example: "他跟店员讨论哪一台比较好用。(He discussed with the clerk which one is better to use.)" },
+
+  { word: "好用", meaning: "useful; handy", example: "这台真的很好用。(This one is really easy to use.)" },
+
+  { word: "新闻", meaning: "news", example: "我看到新闻说这个牌子的电池很耐用。(I saw on the news that this brand's battery is very durable.)" },
+
+  { word: "抱怨", meaning: "to complain", example: "有些客人抱怨电池不太正常。(Some customers complained that the battery isn’t working well.)" },
+
+  { word: "电池", meaning: "battery", example: "这台笔电的电池可以用八个小时。(This laptop's battery lasts eight hours.)" },
+
+  { word: "正常", meaning: "normal; functioning correctly", example: "店员说电池是正常的。(The clerk said the battery is normal.)" },
+
+  { word: "保证", meaning: "guarantee; warranty", example: "这个产品保证一年。(This product has a one-year warranty.)" },
+
+  { word: "修理", meaning: "to repair; fix", example: "如果坏了，可以免费修理。(If it breaks, it can be repaired for free.)" },
+
+  { word: "恐怕", meaning: "I’m afraid that...; probably", example: "恐怕今天没有你要的颜色。(I’m afraid we don’t have the color you want today.)" },
+
+  { word: "改天", meaning: "another day; some other time", example: "我们改天再来看吧。(Let’s come back another day to look.)" },
+
+  { word: "传真机", meaning: "fax machine", example: "公司还有一台传真机。(The company still has a fax machine.)" },
+
+  { word: "传真", meaning: "to fax; fax", example: "请把文件传真给我。(Please fax the document to me.)" },
+
+  { word: "打折", meaning: "to have a discount", example: "这台笔电今天有打折。(This laptop is on discount today.)" },
+
+  { word: "超过", meaning: "to exceed; to be more than", example: "这台超过我的预算。(This one is beyond my budget.)" },
+
+  { word: "把握", meaning: "to seize (chance); to make good use of", example: "要把握今天的折扣。(You should take advantage of today’s discount.)" },
+
+  { word: "大约", meaning: "approximately; about", example: "这台大约三万元。(This one is about 30,000 NT.)" },
+
+  { word: "能够", meaning: "be able to; can", example: "这台能够跑很多程序。(This one can run many programs.)" },
+
+  { word: "真", meaning: "real; genuine", example: "你真懂得挑东西。(You really know how to choose things.)" },
+
+  { word: "皮", meaning: "leather", example: "这个皮的背包很耐用。(This leather backpack is very durable.)" },
+
+  { word: "上班族", meaning: "office worker; working people", example: "上班族常需要带笔电出门。(Office workers often need to bring laptops out.)" },
+
+  { word: "保证书", meaning: "warranty", example: "记得带保证书回去。(Remember to take the warranty card with you.)" }
+];
+
